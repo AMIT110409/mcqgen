0243385b1ac27347ae9ec5aea70141018ab714fd
Amit Rathore <amitrathore110409@gmail.com>
2024-05-14 10:28:34 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 ## this we initalize to install local package in environment . 
+
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Amit Rathore',
+    author_email='amitrathore110409@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
